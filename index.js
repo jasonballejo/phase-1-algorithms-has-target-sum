141af3471c1b6dcc2c68448d605d1555fecd1499
Jason Ballejo <ballejo19@gmail.com>
2022-08-24 23:07:59 -0400

    commit tree 7358162c278e6595eb74b1af5615cb692e3444b6

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
+// function hasTargetSum(array, target) {
+//   // iterate through the array
+//   for (let i = 0; i < array.length; i++) {
+
+//     const complement = target - array[i]
+//     // iterate through the rest of array
+//     for (let j = i + 1; j < array.length; j++) {
+
+//       if (array[j] === complement) return true
+//       }
+//     }
+//       return false
+//   }
+
+// function hasTargetSum(array, target) {
+//   const seenNumbers = {}
+//   for(let i = 0; i < array.length; i++){
+//     console.log(seenNumbers)
+//     const complement = target - array[i]
+//     if (seenNumbers[complement]) return true
+//     seenNumbers[array[i]] = true
+//   }
+//     return false
+// }
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {}
+  for(const number of array){
+    console.log(seenNumbers)
+    const complement = target - number
+    if (complement in seenNumbers) return true
+    seenNumbers[number] = true
+  }
+    return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Big O time complexity of "0(n^2)"
 */
 
 /* 
-  Add your pseudocode here
+  If any pair of array adds up to the target number "10",
+    return true. So we will need to iterate through the numbers to see which two add to the target number.
 */
 
 /*
